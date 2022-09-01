85d71e3897c40206e318041f71615cc901cf0f87
Christian <burkhart.christian@gmail.com>
2022-09-01 13:16:54 +0200

    chore: init project

---
+++ analysis/datenbereinigung.R
@@ -0,0 +1,14 @@
+# (Installiere und) lade folgende Pakete
+library(janitor)
+library(haven)
+
+# 1.0 Daten einlesen ------------------------------------------------------
+
+
+# 1.1 Daten bereinigen ---------------------------------------------
+
+
+
+# 1.4 Datenexport ---------------------------------------------------------
+
+
